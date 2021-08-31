66651afbb316bca8be8085d9b70d9b8d6c24dd7d
Tim Arnold <tim@reachtim.com>
2021-08-31 08:24:45 -0400

    fix typo

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@
 import os
 import sys
 sys.path.append(os.curdir)
-from pelicanconf import *s
+from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://reachtim.com'
