6529feeb1318ed83a1bc1e460d2946914aad3775
Tim Arnold <tim@reachtim.com>
2021-08-31 08:22:10 -0400

    trial push

--- publishconf.py
+++ publishconf.py
@@ -7,11 +7,11 @@
 import os
 import sys
 sys.path.append(os.curdir)
-from pelicanconf import *
+from pelicanconf import *s
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://reachtim.com'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
